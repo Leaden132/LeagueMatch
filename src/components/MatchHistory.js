9d1578228c6e69d5a391fb3557df8fe7c77dc942
Seunggi Lee <seunggi.lee21@gmail.com>
2021-05-31 16:34:39 -0400

    api call cleanup

--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -1,6 +1,6 @@
 import convertChampions from "./covertChampions.js"
 
-const MatchHistory = ({ matchInfo, matchDetailArray, champArray}) => {
+const MatchHistory = ({ matchInfo, matchDetailArray, champArray, getAccountId}) => {
   console.log(matchInfo);
 
   let participants = matchInfo.map((match) => {
@@ -55,46 +55,46 @@ const MatchHistory = ({ matchInfo, matchDetailArray, champArray}) => {
               <div className={`blueTeam${index} blue`}>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[0].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[0].championId, champArray)}></img>
-                  {player[0].summonerName} - {convertChampions(champion[0].championId, champArray)}
+                  <a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[0].summonerName} </a> - {convertChampions(champion[0].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[1].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[1].championId, champArray)}></img>
-                  {player[1].summonerName} - {convertChampions(champion[1].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[1].summonerName} </a>} - {convertChampions(champion[1].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[2].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[2].championId, champArray)}></img>
-                  {player[2].summonerName} - {convertChampions(champion[2].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[2].summonerName} </a>} - {convertChampions(champion[2].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[3].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[3].championId, champArray)}></img>
-                  {player[3].summonerName} - {convertChampions(champion[3].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[3].summonerName} </a>} - {convertChampions(champion[3].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[4].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[4].championId, champArray)}></img>
-                  {player[4].summonerName} - {convertChampions(champion[4].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[4].summonerName} </a>} - {convertChampions(champion[4].championId, champArray)}
                 </div>
               </div>
 
               <div className={`redTeam${index} red`}>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[5].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[5].championId, champArray)}></img>
-                  {player[5].summonerName} - {convertChampions(champion[5].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[5].summonerName} </a>} - {convertChampions(champion[5].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[6].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[6].championId, champArray)}></img>
-                  {player[6].summonerName} - {convertChampions(champion[6].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[6].summonerName} </a>} - {convertChampions(champion[6].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[7].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[7].championId, champArray)}></img>
-                  {player[7].summonerName} - {convertChampions(champion[7].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[7].summonerName} </a>} - {convertChampions(champion[7].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[8].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[8].championId, champArray)}></img>
-                  {player[8].summonerName} - {convertChampions(champion[8].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[8].summonerName} </a>} - {convertChampions(champion[8].championId, champArray)}
                 </div>
                 <div>
                 <img src={`https://opgg-static.akamaized.net/images/lol/champion/${convertChampions(champion[9].championId, champArray)}.png?image=c_scale,q_auto,w_46&amp;v=1612855207`} alt={convertChampions(champion[9].championId, champArray)}></img>
-                  {player[9].summonerName} -{convertChampions(champion[9].championId, champArray)}
+                  {<a href="#" onClick={()=>{getAccountId(player[0].summonerName)}}>{player[9].summonerName} </a>} -{convertChampions(champion[9].championId, champArray)}
                 </div>
               </div>
             </div>
@@ -102,61 +102,6 @@ const MatchHistory = ({ matchInfo, matchDetailArray, champArray}) => {
         );
       })}
 
-      <div className="accountInfo"> </div>
-
-      <div className="rankedInfo"> </div>
-
-      <div className="matchHistory">
-        <div className="game0 game">
-          <div className="blueTeam0 blue"></div>
-          <div className="redTeam0 red"></div>
-        </div>
-
-        <div className="game1 game">
-          <div className="blueTeam1 blue"></div>
-          <div className="redTeam1 red"></div>
-        </div>
-
-        <div className="game2 game">
-          <div className="blueTeam2 blue"></div>
-          <div className="redTeam2 red"></div>
-        </div>
-
-        <div className="game3 game">
-          <div className="blueTeam3 blue"></div>
-          <div className="redTeam3 red"></div>
-        </div>
-
-        <div className="game4 game">
-          <div className="blueTeam4 blue"></div>
-          <div className="redTeam4 red"></div>
-        </div>
-
-        <div className="game5 game">
-          <div className="blueTeam5 blue"></div>
-          <div className="redTeam5 red"></div>
-        </div>
-
-        <div className="game6 game">
-          <div className="blueTeam6 blue"></div>
-          <div className="redTeam6 red"></div>
-        </div>
-
-        <div className="game7 game">
-          <div className="blueTeam7 blue"></div>
-          <div className="redTeam7 red"></div>
-        </div>
-
-        <div className="game8 game">
-          <div className="blueTeam8 blue"></div>
-          <div className="redTeam8 red"></div>
-        </div>
-
-        <div className="game9 game">
-          <div className="blueTeam9 blue"></div>
-          <div className="redTeam9 red"></div>
-        </div>
-      </div>
     </>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = ({handleSubmit}) => {
 <form action="submit" onSubmit={handleSubmit}>
   <label htmlFor="searchInput">
     Summoner Name:
-    <input type="text" name="name" placeholder="Summoner Name" className="searchInput"/>
+    <input type="text" name="summonerName" placeholder="Summoner Name" className="searchInput" defaultValue="TFblade"/>
   </label>
   <input type="submit" value="Search" />
 </form>
