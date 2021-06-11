69b9ef1e2fb4f002bea44a9a8ba929e9871970e5
Seunggi Lee <seunggi.lee21@gmail.com>
2021-06-10 23:51:16 -0400

    code cleanup

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 
 const SearchBar = () => {
