2d0daa311c2f7407eb3a5a004a79389baa023c9e
Leaden132 <seunggi.lee21@gmail.com>
2021-06-15 18:27:22 -0400

    rune function fix

--- src/components/convertRunes.tsx
+++ src/components/convertRunes.tsx
@@ -4,22 +4,13 @@ const convertRunes = (runeId:number, runeArray:any):string => {
     let runeURL = 'perk-images/Styles/7200_Domination.png';
 
     for (let i=0; i< runeArray.length;i++) {
-        runeArray[i].slots[0].runes.forEach((rune:any, index:number)=>{
-            if (rune.id===runeId){
-                runeURL=rune.icon;
+        for(let x=0; x< runeArray[i].slots[0].runes.length; x++){
+            if (runeArray[i].slots[0].runes[x].id===runeId){
+                runeURL=runeArray[i].slots[0].runes[x].icon;
             }
-        })
-        // if (runeArray[i].slots[0].runes[0].id === runeId){
- 
-        // }
+        }
     }
 
-//   const championArray = Object.keys(champObj);
-//       for(let i = 0 ; i <championArray.length ; i++){
-//           if (parseInt(champObj[championArray[i]].key, 10) === championId){
-//               championName = championArray[i];
-//           }
-//       }
       return runeURL;
 }
 
